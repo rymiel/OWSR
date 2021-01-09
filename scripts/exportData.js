cd996727b66a87f87145bbfd7d986acf7e3e5bb5
rymiel <rymiel@users.noreply.github.com>
2021-01-09 16:52:19 +0000

    deploy: ad3a185652702a872571fd4a35c3febaea5c9ceb

---
+++ scripts/exportData.js
@@ -0,0 +1,14 @@
+import { setDirtyState } from "./DirtyState";
+import { getItems } from "./ItemManager";
+export function exportEntries() {
+    const text = JSON.stringify(getItems());
+    download(text, 'entries.txt', 'text/plain');
+    setDirtyState(false);
+}
+function download(content, fileName, contentType) {
+    const a = document.createElement("a");
+    const file = new Blob([content], { type: contentType });
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+}
---
+++ scripts/handler.js
@@ -0,0 +1,51 @@
+import { loadFromLocalStorage } from "./ItemManager";
+import { getDirtyState, initIgnoreDirtyState, setDirtyState } from "./DirtyState";
+import { initCharts, updateCharts } from "./GraphManager";
+import { updateStats, updateInfo, updateSeasonSelect, updateWLD } from "./InfoManager";
+import { addRow, rebuildTable } from "./TableManager";
+import { importEntries } from "./importData";
+import { exportEntries } from "./exportData";
+export function init() {
+    window.addEventListener("load", () => {
+        initIgnoreDirtyState(document.querySelector("#cbxDirtyState"));
+        loadFromLocalStorage();
+        initCharts();
+        rebuildTable();
+        setDirtyState(false);
+        window.dispatchEvent(new Event("updateAll"));
+    });
+    window.addEventListener("beforeunload", (event) => {
+        if (getDirtyState()) {
+            const message = 'You may export the latest changes';
+            event.returnValue = message;
+            return message;
+        }
+    });
+    window.addEventListener("updateAll", () => {
+        updateSeasonSelect();
+        updateCharts();
+        updateStats();
+        updateInfo();
+        updateWLD();
+    });
+    window.updateStats = updateStats;
+    window.updateSeasonStats = () => {
+        updateCharts();
+    };
+    window.addRow = addRow;
+    window.exportEntries = exportEntries;
+    window.importEntries = importEntries;
+    window.setProgressChart = (event) => {
+        // Reset Charts
+        const progressCharts = ["ctxProgress1", "ctxProgress2"];
+        progressCharts.forEach(value => {
+            const element = document.getElementById(value);
+            element.style.display = "none";
+        });
+        // Show correct chart
+        const select = document.getElementById("progressSelect");
+        const value = select.value;
+        const element = document.getElementById(value);
+        element.style.display = "";
+    };
+}
